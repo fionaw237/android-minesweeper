80e0bec9ebce87691dee38d8544f06b84256b998
Fiona Wilson <fiona.wilson@resdiary.com>
2019-11-25 13:35:45 +0000

    Added basic navigation between fragments

--- app/src/main/java/com/example/android_minesweeper/WelcomeScreenFragment.kt
+++ app/src/main/java/com/example/android_minesweeper/WelcomeScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android_minesweeper.databinding.WelcomeScreenBinding
 
 class WelcomeScreenFragment : Fragment() {
@@ -14,7 +15,26 @@ class WelcomeScreenFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.welcome_screen, container, false)
+
+        binding.beginnerButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_welcomeScreenFragment_to_gameScreenFragment)
+        }
+
+        binding.intermediateButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_welcomeScreenFragment_to_gameScreenFragment)
+        }
+
+        binding.advancedButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_welcomeScreenFragment_to_gameScreenFragment)
+        }
+
+        binding.bestTimesButton.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_welcomeScreenFragment_to_bestTimesFragment)
+        }
+
         return binding.root
     }
+
+
 
 }
