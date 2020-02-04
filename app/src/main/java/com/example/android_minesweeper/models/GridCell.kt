12e880f2c01b342c28d91a019737d6c28ee95379
Fiona Wilson <fiona.wilson@resdiary.com>
2020-02-04 16:56:04 +0000

    Added functionality to highlight clicked cell and show all mines when it's game over

--- app/src/main/java/com/example/android_minesweeper/models/GridCell.kt
+++ app/src/main/java/com/example/android_minesweeper/models/GridCell.kt
@@ -4,5 +4,14 @@ class GridCell(
     var hasMine: Boolean = false,
     var hasFlag: Boolean = false,
     var uncovered: Boolean = false,
-    val positionInGrid: Int
-)
+    val positionInGrid: Int,
+    var cellClickedForGameOver: Boolean = false) {
+
+    fun configureForGameOver() {
+        if (hasMine && !hasFlag) {
+            uncovered = true
+        } //else if (!cell.hasMine && cell.hasFlag) {
+
+        // }
+    }
+}
--- app/src/main/java/com/example/android_minesweeper/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android_minesweeper/screens/game/GameViewModel.kt
@@ -74,25 +74,46 @@ class GameViewModel(private val difficulty: Difficulty) : BaseViewModel() {
     }
 
     fun gridCellTapped(gridCell: GridCell) {
+        if (gridCell.uncovered || gridCell.hasFlag) return
+
         if (!timerStarted) {
             randomlyDistributeMines()
             timerStarted = true
         }
 
-        if (!gridCell.uncovered) {
-            gridCells.find { it == gridCell }?.let { cell ->
-                cell.uncovered = true
+        gridCells.find { it == gridCell }?.let { cell ->
+            cell.uncovered = true
+            if (cell.hasMine) {
+                gameOver(clickedCell = cell)
+                return
             }
-            refreshGridCells(gridCells)
         }
+
+        refreshGridCells(gridCells)
     }
 
     private fun refreshGridCells(updatedGridCells: MutableList<GridCell>) {
         gridCells = updatedGridCells
     }
 
+    private fun gameOver(clickedCell: GridCell) {
+        clickedCell.cellClickedForGameOver = true
+        showAllMines()
+        // disable user interaction on all cells
+        // invalidate timer
+        // play sound
+        // configure reset button
+        refreshGridCells(gridCells)
+    }
+
     fun resetGrid() {
         setUpGame()
+    }
+
+    private fun showAllMines() {
+        gridCells.forEach { cell ->
+            cell.configureForGameOver()
+        }
     }
 
     private fun setFlagsLabel() {
